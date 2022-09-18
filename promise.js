012cd4b79232baa86815a037261382534f49e1e0
Hadis Topan Trisakti <hadistopan@gmail.com>
2022-09-18 22:28:57 +0700

    editing promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (mood) => {
+  try {
+    const bioskopX = await promiseTheaterIXX();
+    const bioskopV = await promiseTheaterVGC();
+    const allBioskop = [...bioskopX, ...bioskopV];
+
+    const arrMood = allBioskop.filter((el) => el.hasil === mood);
+    let jmlMood = `${mood} : ${arrMood.length}`;
+
+    return jmlMood;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
